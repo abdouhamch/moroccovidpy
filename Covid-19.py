5d470b015ef3308925bc8cf26acc560f993f7de1
abdouhamch <59808422+abdouhamch@users.noreply.github.com>
2020-03-23 18:23:28 +0100

    Update Covid-19.py

--- Covid-19.py
+++ Covid-19.py
@@ -8,12 +8,17 @@ Recovered = morocco.status('Recovered')
 Today_Confirmed = morocco.status('TodayConfirmed')
 Today_Deaths = morocco.status('TodayDeaths')
 
+
 # Create List
-Val = [Confirmed,Deaths,Recovered,Today_Confirmed,Today_Deaths]
+Val = [Confirmed, Deaths, Recovered, Today_Confirmed, Today_Deaths]
 Labels = ["Confirmed","Deaths","Recovered","Today Confirmed","Today Deaths"]
 
 # Create Charts
-plt.pie(Val, labels=Labels)
-plt.show()
-
-
+if Today_Deaths == 0:
+    Val = [Confirmed, Deaths, Recovered, Today_Confirmed]
+    Labels = ["Confirmed", "Deaths", "Recovered", "Today Confirmed"]
+    plt.pie(Val, labels=Labels, radius=1.4, autopct='%0.0f%%')
+    plt.show()
+else:
+    plt.pie(Val, labels=Labels, radius=1.4, autopct='%0.0f%%')
+    plt.show()
